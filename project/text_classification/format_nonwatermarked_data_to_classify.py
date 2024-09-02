024878e9d5a37e224f6b06cc5cfe37fd8ccf5ad8
youssefkhalil320 <youssefkhalil320@gmail.com>
2024-09-02 23:34:33 +0300

    edit format script

--- project/text_classification/format_nonwatermarked_data_to_classify.py
+++ project/text_classification/format_nonwatermarked_data_to_classify.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 # Load the datasets
-flan_df = pd.read_csv('../data/cnn_dailymail_flan_t5_dataset.csv')
-t5_df = pd.read_csv('../data/cnn_dailymail_t5_dataset.csv')
+flan_df = pd.read_csv('../data/cnn_dailymail-watermarked-flan-t5-small_dataset.csv')
+t5_df = pd.read_csv('../data/cnn_dailymail-watermarked-t5-small_dataset.csv')
 
 
 
 # Create the second combined dataset with watermarked summaries
 watermarked_summary_combined_df = pd.DataFrame({
-    'text': pd.concat([t5_df['t5_summary'], flan_df['flan_t5_summary']], ignore_index=True),
+    'text': pd.concat([t5_df['watermarked_summary'], flan_df['watermarked_summary']], ignore_index=True),
     'label': [0] * len(t5_df) + [1] * len(flan_df)
 })
 
@@ -20,4 +20,4 @@ watermarked_summary_combined_df = watermarked_summary_combined_df.sample(frac=1)
 print(watermarked_summary_combined_df.shape)
 
 # Save the new datasets
-watermarked_summary_combined_df.to_csv('new_data/cnn_dailymail_summary_unwatermarked_dataset.csv', index=False)
+watermarked_summary_combined_df.to_csv('new_data/cnn_watermarked_dataset.csv', index=False)
